840fd3a311ec48dfd357d53bc14b785b9c574549
Justin Britt <britt@uwindsor.ca>
2023-06-10 04:56:06 -0400

    Added abstract factory

---
+++ HM.HM3A.A.E.O/InterfacesAbstractFactories/IComparersAbstractFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesAbstractFactories
+{
+    using HM.HM3A.A.E.O.InterfacesFactories.Comparers;
+
+    public interface IComparersAbstractFactory
+    {
+        IOrganizationComparerFactory CreateOrganizationComparerFactory();
+    }
+}
