c79b91b5c77dd818908e715ca9812f8c4a29761a
Justin Britt <britt@uwindsor.ca>
2023-06-09 21:21:30 -0400

    Implement IComparable

--- HM.HM3A.A.E.O/Classes/IndexElements/jIndexElement.cs
+++ HM.HM3A.A.E.O/Classes/IndexElements/jIndexElement.cs
@@ -1,5 +1,7 @@
 ﻿namespace HM.HM3A.A.E.O.Classes.IndexElements
 {
+    using System;
+
     using log4net;
 
     using Hl7.Fhir.Model;
@@ -20,5 +22,13 @@
         /// Gets the surgical specialty.
         /// </summary>
         public Organization Value { get; }
+
+        public int CompareTo(
+            IjIndexElement other)
+        {
+            return String.CompareOrdinal(
+                other.Value.Id,
+                this.Value.Id);
+        }
     }
 }
