c0db56da0ca5ca7075c1776e7fda3198d59427dc
Justin Britt <britt@uwindsor.ca>
2023-06-14 07:07:29 -0400

    Added class

---
+++ HM.HM3A.A.E.O/Classes/Comparers/DeviceComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace HM.HM3A.A.E.O.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM3A.A.E.O.Interfaces.Comparers;
+
+    internal sealed class DeviceComparer : IDeviceComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DeviceComparer()
+        {
+        }
+
+        public int Compare(
+            Device x,
+            Device y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
