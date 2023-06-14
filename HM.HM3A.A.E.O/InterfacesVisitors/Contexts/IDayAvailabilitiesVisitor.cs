bb08dc55970a828b2ba5f945fec9786b77811970
Justin Britt <britt@uwindsor.ca>
2023-06-14 06:56:11 -0400

    Added visitor

---
+++ HM.HM3A.A.E.O/InterfacesVisitors/Contexts/IDayAvailabilitiesVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3A.A.E.O.Interfaces.IndexElements;
+    using HM.HM3A.A.E.O.Interfaces.ParameterElements.DayAvailabilities;
+
+    public interface IDayAvailabilitiesVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : FhirDateTime
+        where TValue : INullableValue<bool>
+    {
+        RedBlackTree<ItIndexElement, IψParameterElement> RedBlackTree { get; }
+    }
+}
