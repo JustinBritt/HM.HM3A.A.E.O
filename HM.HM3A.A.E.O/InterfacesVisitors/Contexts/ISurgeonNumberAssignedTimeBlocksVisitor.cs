499a46bafd5f0413990a4c2ebddb82fe96d65918
Justin Britt <britt@uwindsor.ca>
2023-06-16 08:17:58 -0400

    Added visitor

---
+++ HM.HM3A.A.E.O/InterfacesVisitors/Contexts/ISurgeonNumberAssignedTimeBlocksVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesVisitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3A.A.E.O.Interfaces.IndexElements;
+    using HM.HM3A.A.E.O.Interfaces.ParameterElements.SurgeonNumberAssignedTimeBlocks;
+
+    public interface ISurgeonNumberAssignedTimeBlocksVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IsIndexElement, IBParameterElement> RedBlackTree { get; }
+    }
+}
