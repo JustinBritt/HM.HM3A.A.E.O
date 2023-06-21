84b66775c78123fee276c7c96d1bc1e3fdd3e538
Justin Britt <britt@uwindsor.ca>
2023-06-21 13:28:05 -0400

    Added visitor

---
+++ HM.HM3A.A.E.O/Visitors/Results/SurgeonOperatingRoomAssignments/yOuterVisitor.cs
@@ -0,0 +1,76 @@
+﻿namespace HM.HM3A.A.E.O.Visitors.Results.SurgeonOperatingRoomAssignments
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using HM.HM3A.A.E.O.Interfaces.Comparers;
+    using HM.HM3A.A.E.O.Interfaces.IndexElements;
+    using HM.HM3A.A.E.O.Interfaces.Indices;
+    using HM.HM3A.A.E.O.Interfaces.ResultElements.SurgeonOperatingRoomAssignments;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Results.SurgeonOperatingRoomAssignments;
+
+    internal sealed class yOuterVisitor<TKey, TValue> : IyOuterVisitor<TKey, TValue>
+        where TKey : IsIndexElement
+        where TValue : RedBlackTree<IrIndexElement, IyResultElement>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public yOuterVisitor(
+            INullableValueFactory nullableValueFactory,
+            IDeviceComparer deviceComparer,
+            ILocationComparer locationComparer,
+            IOrganizationComparer organizationComparer,
+            Ir r,
+            Is s)
+        {
+            this.NullableValueFactory = nullableValueFactory;
+
+            this.LocationComparer = locationComparer;
+
+            this.r = r;
+
+            this.s = s;
+
+            this.RedBlackTree = new RedBlackTree<Organization, RedBlackTree<Location, INullableValue<bool>>>(
+                organizationComparer);
+        }
+
+        private INullableValueFactory NullableValueFactory { get; }
+
+        private ILocationComparer LocationComparer { get; }
+
+        private Ir r { get; }
+
+        private Is s { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<Organization, RedBlackTree<Location, INullableValue<bool>>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IsIndexElement sIndexElement = obj.Key;
+
+            RedBlackTree<IrIndexElement, IyResultElement> value = obj.Value;
+
+            var innerVisitor = new yInnerVisitor<IrIndexElement, IyResultElement>(
+                this.NullableValueFactory,
+                this.LocationComparer);
+
+            value.AcceptVisitor(
+                innerVisitor);
+
+            this.RedBlackTree.Add(
+                sIndexElement.Value,
+                innerVisitor.RedBlackTree);
+        }
+    }
+}
