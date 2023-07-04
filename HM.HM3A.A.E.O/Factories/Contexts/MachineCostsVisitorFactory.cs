70ef029b6b9f592ab2b1625885cf0734ed60ad1b
Justin Britt <britt@uwindsor.ca>
2023-07-04 05:12:59 -0400

    Added factory

---
+++ HM.HM3A.A.E.O/Factories/Contexts/MachineCostsVisitorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace HM.HM3A.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM3A.A.E.O.Interfaces.Indices;
+    using HM.HM3A.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using HM.HM3A.A.E.O.InterfacesFactories.ParameterElements.MachineCosts;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM3A.A.E.O.Visitors.Contexts;
+
+    internal sealed class MachineCostsVisitorFactory : IMachineCostsVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public MachineCostsVisitorFactory()
+        {
+        }
+
+        public IMachineCostsVisitor<TKey, TValue> Create<TKey, TValue>(
+            IRedBlackTreeFactory redBlackTreeFactory,
+            ICParameterElementFactory CParameterElementFactory,
+            Im m)
+            where TKey : Device
+            where TValue : Money
+        {
+            IMachineCostsVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new MachineCostsVisitor<TKey, TValue>(
+                    redBlackTreeFactory,
+                    CParameterElementFactory,
+                    m);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM3A.A.E.O/InterfacesFactories/Contexts/IMachineCostsVisitorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM3A.A.E.O.Interfaces.Indices;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using HM.HM3A.A.E.O.InterfacesFactories.ParameterElements.MachineCosts;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface IMachineCostsVisitorFactory
+    {
+        IMachineCostsVisitor<TKey, TValue> Create<TKey, TValue>(
+            IRedBlackTreeFactory redBlackTreeFactory,
+            ICParameterElementFactory CParameterElementFactory,
+            Im m)
+            where TKey : Device
+            where TValue : Money;
+    }
+}
