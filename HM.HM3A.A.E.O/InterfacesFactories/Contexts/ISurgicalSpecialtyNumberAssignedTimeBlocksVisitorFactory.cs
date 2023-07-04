8e0de69fee085db6771922ae10ceb76b5ef4671c
Justin Britt <britt@uwindsor.ca>
2023-07-04 07:25:53 -0400

    Added factory

---
+++ HM.HM3A.A.E.O/InterfacesFactories/Contexts/ISurgicalSpecialtyNumberAssignedTimeBlocksVisitorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM3A.A.E.O.Interfaces.Indices;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using HM.HM3A.A.E.O.InterfacesFactories.ParameterElements.SurgicalSpecialtyNumberAssignedTimeBlocks;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgicalSpecialtyNumberAssignedTimeBlocksVisitorFactory
+    {
+        ISurgicalSpecialtyNumberAssignedTimeBlocksVisitor<TKey, TValue> Create<TKey, TValue>(
+            IRedBlackTreeFactory redBlackTreeFactory,
+            IBParameterElementFactory BParameterElementFactory,
+            Ij j)
+            where TKey : Organization
+            where TValue : INullableValue<int>;
+    }
+}
