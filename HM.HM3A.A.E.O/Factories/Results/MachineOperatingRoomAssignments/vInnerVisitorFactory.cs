96024d726af08d6de057cc1bcb05c9013ea5fc0b
Justin Britt <britt@uwindsor.ca>
2023-07-04 08:39:36 -0400

    Added factory

---
+++ HM.HM3A.A.E.O/Factories/Results/MachineOperatingRoomAssignments/vInnerVisitorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace HM.HM3A.A.E.O.Factories.Results.MachineOperatingRoomAssignments
+{
+    using System;
+
+    using log4net;
+
+    using HM.HM3A.A.E.O.Interfaces.Comparers;
+    using HM.HM3A.A.E.O.Interfaces.IndexElements;
+    using HM.HM3A.A.E.O.Interfaces.ResultElements.MachineOperatingRoomAssignments;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using HM.HM3A.A.E.O.InterfacesFactories.Results.MachineOperatingRoomAssignments;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Results.MachineOperatingRoomAssignments;
+    using HM.HM3A.A.E.O.Visitors.Results.MachineOperatingRoomAssignments;
+
+    internal sealed class vInnerVisitorFactory : IvInnerVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public vInnerVisitorFactory()
+        {
+        }
+
+        public IvInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            INullableValueFactory nullableValueFactory,
+            IRedBlackTreeFactory redBlackTreeFactory,
+            ILocationComparer locationComparer)
+            where TKey : IrIndexElement
+            where TValue : IvResultElement
+        {
+            IvInnerVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new vInnerVisitor<TKey, TValue>(
+                    nullableValueFactory,
+                    redBlackTreeFactory,
+                    locationComparer);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM3A.A.E.O/InterfacesFactories/Results/MachineOperatingRoomAssignments/IvInnerVisitorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM3A.A.E.O.InterfacesFactories.Results.MachineOperatingRoomAssignments
+{
+    using HM.HM3A.A.E.O.Interfaces.Comparers;
+    using HM.HM3A.A.E.O.Interfaces.IndexElements;
+    using HM.HM3A.A.E.O.Interfaces.ResultElements.MachineOperatingRoomAssignments;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM3A.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+    using HM.HM3A.A.E.O.InterfacesVisitors.Results.MachineOperatingRoomAssignments;
+
+    public interface IvInnerVisitorFactory
+    {
+        IvInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            INullableValueFactory nullableValueFactory,
+            IRedBlackTreeFactory redBlackTreeFactory,
+            ILocationComparer locationComparer)
+            where TKey : IrIndexElement
+            where TValue : IvResultElement;
+    }
+}
